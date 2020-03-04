f415f2a351d5e4f420d8da93a5b47517aa0a69e8
Devin <devinfrench@users.noreply.github.com>
2020-03-04 10:24:12 -0800

    Add webpack, babel, and react

---
+++ project/vibecheck/src/components/App.js
@@ -0,0 +1,26 @@
+import React, { Component } from "react";
+import { render } from "react-dom";
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: [],
+      loaded: false,
+      placeholder: "Loading"
+    };
+  }
+
+  componentDidMount() {
+    console.log("mounted");
+  }
+
+  render() {
+    return <h1>Hello World</h1>;
+  }
+}
+
+export default App;
+
+const container = document.getElementById("app");
+render(<App />, container);
