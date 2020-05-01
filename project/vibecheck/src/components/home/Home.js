d866442641edbb1570c144f9d06c0039cd94dc68
Devin French <d_french3@u.pacific.edu>
2020-05-01 13:31:53 -0700

    Remove home side nav

--- project/vibecheck/src/components/home/Home.js
+++ project/vibecheck/src/components/home/Home.js
@@ -18,9 +18,7 @@ class Home extends Component {
         <Nav />
         <div className="home-container">
           <div className="row">
-            <div className="column">
-              <SideNav />
-            </div>
+            <div className="column"></div>
             <div className="column">
               <Feed />
             </div>
